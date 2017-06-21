1e28b076e1e4cc0d894b88f239d0ea56ed10729a
Иванов Никита <b.m.v.12@mail.ru>
2017-06-21 14:36:33 +0300

    Slozenie dvuh chisel

---
+++ P1.pas
@@ -0,0 +1,23 @@
+Program Vvod;
+
+Var 
+
+  n1, n2, c:Integer;
+
+  X:Real;
+
+Begin
+
+Write('nl=');
+
+Readln(n1);
+
+Write('n2=');
+
+Readln(n2);
+
+c:=n1+n2;
+
+Write ('Сумма равна ',c);
+
+End.
